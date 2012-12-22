7c91359dd30f89bd0d7197350b2a0769b1e76231
Dan Cuellar <danc@zoosk.com>
2012-12-22 08:29:37 -0600

    renaming variables in bitbeambot class to be more specific

--- appium/robot/bitbeambot.py
+++ appium/robot/bitbeambot.py
@@ -22,44 +22,44 @@ from time import sleep
 
 class Bot():
 
-    class Servo:
+    class Arm:
         def __init__(self, bot, num):
             self.bot = bot
-            self.arm_number = num
-            self.position = 30
+            self.number = num
+            self.angle = 30
 
-        def set_position(self, new_position):
-            self.bot.send_command(self.arm_number, new_position)
+        def set_angle(self, new_position):
+            self.bot.send_command(self.number, new_position)
             self.bot.position = new_position
 
-        def move(self, new_position, delay=0.03, max_movement=1000):
-            positions = []
-            if self.position < new_position:
-                positions = range(self.position, new_position)
-            elif new_position < self.position:
-                positions = range(self.position, new_position, -1)
-            if len(positions) > max_movement:
-                positions = positions[:max_movement]
-            for position in positions:
-                self.set_position(position)
+        def move(self, desired_angle, delay=0.03, max_movement=1000):
+            intermediate_angles = []
+            if self.angle < desired_angle:
+                intermediate_angles = range(self.angle, desired_angle)
+            elif desired_angle < self.angle:
+                intermediate_angles = range(self.angle, desired_angle, -1)
+            if len(intermediate_angles) > max_movement:
+                intermediate_angles = intermediate_angles[:max_movement]
+            for angle in intermediate_angles:
+                self.set_angle(angle)
                 sleep(delay)
 
     def __init__(self, serial_port):
         self.serial = serial.Serial(serial_port,9600,timeout=1)
-        self.a = Bot.Servo(self, "1")
-        self.b = Bot.Servo(self, "2")
-        self.c = Bot.Servo(self, "3")
+        self.a = Bot.Arm(self, "1")
+        self.b = Bot.Arm(self, "2")
+        self.c = Bot.Arm(self, "3")
 
     def position(self):
-        return (self.a.position, self.b.position, self.c.position)
+        return (self.a.angle, self.b.angle, self.c.angle)
 
     def send_command(self, command, position='0'):
         self.serial.write(command)
         self.serial.write( chr(position))
 
-    def move(self, a_position, b_position, c_position, delay=0.03):
-        while self.position() is not (a_position, b_position, c_position):
-            self.a.move(a_position,0,1)
-            self.b.move(b_position,0,1)
-            self.c.move(c_position,0,1)
+    def move(self, a_angle, b_angle, c_angle, delay=0.03):
+        while self.position() is not (a_angle, b_angle, c_angle):
+            self.a.move(a_angle,0,1)
+            self.b.move(b_angle,0,1)
+            self.c.move(c_angle,0,1)
             sleep(delay)
