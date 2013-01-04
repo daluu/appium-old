5e105521cd1fb01dff40e49414584c067b3cb701
Dan Cuellar <penguinho@aol.com>
2013-01-03 17:13:01 -0800

    Compelted Basic Robot Calibration Routine

--- appium/robot/bitbeambot-d2/calibrate.py
+++ appium/robot/bitbeambot-d2/calibrate.py
@@ -22,22 +22,24 @@ import sys
 from sys import argv
 from time import sleep
 from math import sqrt, pow
-from bitbeambot import Bot
+from robot import Bot
 
-appiumpath = os.path.abspath(os.path.join(os.path.split(os.path.abspath(__file__))[0],'../'))
+appiumpath = os.path.abspath(os.path.join(os.path.split(os.path.abspath(__file__))[0],'../../'))
 sys.path.append(appiumpath)
 import appium
 
 # globals
 bot = None
 driver = None
-starting_position = 30
-current_a = 30
-current_b = 30
-current_c = 30
+starting_position = 40
 contact_point = None
-mappings = []
+origin_point = None
 screen_center = None
+left_point = None
+right_point = None
+up_point = None
+down_point = None
+mappings = []
 
 def distance(x1,y1,x2,y2):
     return sqrt( pow(x2-x1,2) + pow(y2-y1,2) )
@@ -45,7 +47,10 @@ def distance(x1,y1,x2,y2):
 def get_coords():
     global driver
     coords = driver.proxy('mainWindow.staticTexts()[0].value()')
-    if len(coords) < 1:
+    if len(coords[0]) < 2:
+        return None
+    coords = coords[0][1]
+    if len(coords) < 1 or ',' not in coords:
         return None
     return (int(coords.split(',')[0][1:].replace(' ','')), int(coords.split(',')[1][:-1].replace(' ','')) )
 
@@ -72,46 +77,116 @@ def map_screen_point(x,y):
     return  triangulated_point
 
 def print_usage():
-    print 'python calibrate.py /dev/usb-port "/path/to/my.app"'
+    print 'python calibrate.py UDID /dev/usb-port'
 
-if __name__ == '__main__':
-    global bot, driver, starting_position, current_a, current_b, current_c, contact_point, mappings, screen_center
+if len(argv) < 2 or not argv[2].startswith("/dev/"):
+    print_usage()
+    exit(-1)
 
-    if len(argv) < 2 or not argv[1].startswith("/dev/"):
-        print_usage()
-        exit(-1)
+# create a bot instance
+print 'Making robot'
+bot = Bot(argv[2])
 
-    # create a bot instance
-    bot = Bot(argv[1])
+# create an appium instance
+print 'Launching Calibration App'
+driver = appium.Appium('Appium.RobotCalibration', argv[1])
+driver.start()
 
-    # create an appium instance
-    driver = appium.Appium(argv[2])
-    driver.start()
+# lower the pen until the ios device is touched
+current_point = starting_position
+screen_center = None
+print 'Entering Calibration Loop'
+while current_point <= 80 and screen_center == None:
+    current_point += 1
+    # exit if we did not touch the ios device
+    if current_point > 80:
+        raise Exception("Could not contact the iOS device")
+    bot.move(current_point, current_point, current_point)
+    screen_center = get_coords()
 
-    # lower the pen until the ios device is touched
-    current_point = starting_position
-    screenCenter = None
-    while current_point <= 80 and screenCenter == None:
-        current_point += 1
-        # exit if we did not touch the ios device
-        if current_point > 80:
-            raise Exception("Could not contact the iOS device")
-        bot.move(current_point, current_point, current_point)
-        screenCenter = get_coords()
-        contact_point = screen_center
+    contact_point = bot.forward_k(current_point, current_point, current_point)
+    if contact_point[0] is not 0:
+        raise Exception('Could not detect contact point')
+    contact_point = (contact_point[1], contact_point[2], contact_point[3])
 
-    # add a mapping to the screen center
-    mappings.append(screenCenter, (current_a, current_b, current_c))
+    origin_point = bot.forward_k(starting_position, starting_position, starting_position)
+    if origin_point[0] is not 0:
+        raise Exception('Could not detect origin point')
+    origin_point = (origin_point[1], origin_point[2], origin_point[3])
 
-    # return the device to the original position
-    bot.move(starting_position,starting_position,starting_position)
+# add a mapping to the screen center
+mappings.append((screen_center, (bot.a.angle, bot.b.angle, bot.c.angle)))
 
-    # TODO: find the points in +x, -x, +y, and -y directions
-    # TODO: find screen edges
+# return the device to the original position
+bot.move(starting_position,starting_position,starting_position)
 
-    # quit appium
-    driver.stop()
+# touch to the left
+left50pt1 = bot.inverse_k(contact_point[0]-50, 0, origin_point[2])
+left50pt2 = bot.inverse_k(contact_point[0]-50, 0, contact_point[2])
+bot.set_position(left50pt1)
+sleep(1.0)
+bot.set_position(left50pt2)
+left_coords = get_coords()
+left_position = bot.position()
+left_point = bot.forward_k(left_position[0], left_position[1], left_position[2])
+if left_point[0] is not 0:
+    raise Exception('Could not detect origin point')
+left_point = (left_point[1], left_point[2], left_point[3])
+mappings.append((left_coords, left_point))
 
-    # print summary of calibration
-    print 'Contact Point : ' + str(contact_point)
-    print 'Screen Center : (' + str(screen_center[0]) + ',' + str(screen_center[1]) + ')'
+# touch to the right
+right50pt1 = bot.inverse_k(contact_point[0]+50, 0, origin_point[2])
+right50pt2 = bot.inverse_k(contact_point[0]+50, 0, contact_point[2])
+bot.set_position(right50pt1)
+sleep(1.0)
+bot.set_position(right50pt2)
+right_coords = get_coords()
+right_position = bot.position()
+right_point = bot.forward_k(right_position[0], right_position[1], right_position[2])
+if right_point[0] is not 0:
+    raise Exception('Could not detect origin point')
+right_point = (right_point[1], right_point[2], right_point[3])
+mappings.append((right_coords, right_point))
+
+# touch to the top
+up50pt1 = bot.inverse_k(0, contact_point[0]+50, origin_point[2])
+up50pt2 = bot.inverse_k(0, contact_point[0]+50, contact_point[2])
+bot.set_position(up50pt1)
+sleep(1.0)
+bot.set_position(up50pt2)
+up_coords = get_coords()
+up_position = bot.position()
+up_point = bot.forward_k(up_position[0], up_position[1], up_position[2])
+if up_point[0] is not 0:
+    raise Exception('Could not detect origin point')
+up_point = (up_point[1], up_point[2], up_point[3])
+mappings.append((up_coords, up_point))
+
+# touch to the bottom
+down50pt1 = bot.inverse_k(0, contact_point[0]-50, origin_point[2])
+down50pt2 = bot.inverse_k(0, contact_point[0]-50, contact_point[2])
+bot.set_position(down50pt1)
+sleep(1.0)
+bot.set_position(down50pt2)
+down_coords = get_coords()
+down_position = bot.position()
+down_point = bot.forward_k(down_position[0], down_position[1], down_position[2])
+if down_point[0] is not 0:
+    raise Exception('Could not detect origin point')
+down_point = (down_point[1], down_point[2], down_point[3])
+mappings.append((down_coords, down_point))
+
+# return the device to the original position
+bot.move(starting_position,starting_position,starting_position)
+
+# quit appium
+driver.stop()
+
+# print summary of calibration
+print 'Origin Point : ' + str(origin_point)
+print 'Contact Point : ' + str(contact_point)
+print 'Left Point : ' + str(left_point)
+print 'Right Point : ' + str(right_point)
+print 'Up Point : ' + str(up_point)
+print 'Down Point : ' + str(down_point)
+#print 'Screen Center : (' + str(screen_center[0]) + ', ' + str(screen_center[1]) + ')'
